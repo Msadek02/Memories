f19e3152c209a9d80ce60b37e8a5093d5ae3d7e6
Mostafa Sadek <thunder_axe_90@hotmail.com>
2016-05-17 13:46:55 -0400

    add views and models

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
+
   def index
+    @profile = Profile.new
+  end
+
+  def show
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516191135) do
+ActiveRecord::Schema.define(version: 20160516194525) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.text     "bio",        limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
